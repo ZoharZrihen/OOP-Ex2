df3bc40e9d85b8e71122c7a77cd2148a2ffd37ac
Zohar Zrihen <zohar8395@gmail.com>
2019-12-17 18:02:51 +0200

    second commit

---
+++ dataStructure/node.java
@@ -0,0 +1,53 @@
+package dataStructure;
+
+import utils.Point3D;
+
+public class node implements node_data{
+    private int key;
+    private Point3D location;
+    private double weight;
+    @Override
+    public int getKey() {
+        return 0;
+    }
+
+    @Override
+    public Point3D getLocation() {
+        return null;
+    }
+
+    @Override
+    public void setLocation(Point3D p) {
+
+    }
+
+    @Override
+    public double getWeight() {
+        return 0;
+    }
+
+    @Override
+    public void setWeight(double w) {
+
+    }
+
+    @Override
+    public String getInfo() {
+        return null;
+    }
+
+    @Override
+    public void setInfo(String s) {
+
+    }
+
+    @Override
+    public int getTag() {
+        return 0;
+    }
+
+    @Override
+    public void setTag(int t) {
+
+    }
+}
